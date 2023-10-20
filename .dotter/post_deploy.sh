eda9bd589ac5cf399c0c24fb806dd87029a3c327
George Patterson <georgepatterson@Georges-MacBook-Pro.local>
2023-10-20 15:44:04 -0400

    fix: updates post deploy

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -3,8 +3,8 @@
 # test if zsh works
 echo "Sourcing zsh files"
 exec /bin/zsh
-# /bin/zsh -c 'source ~/.zshenv' || "could not source zshenv"
-# /bin/zsh -c 'source ~/.zshrc' || "could not source zshrc"
+/bin/zsh -c 'source ~/.zshenv' || "could not source zshenv"
+/bin/zsh -c 'source ~/.zshrc' || "could not source zshrc"
 
 # install docker separately
 {{#if (is_executable "docker")}}
